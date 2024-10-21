a0ed713c1a65aee5abfdd121f3266b9b350b87aa
zainab <zainabab789@gmail.com>
2024-10-21 23:31:14 +0500

    first commit

---
+++ add.js
@@ -0,0 +1,15 @@
+// jab hum store ma enter howa hamara bill
+var totalAmount = 0;
+//buy surf
+var surfprice = 500;
+var riceprice = 2000;
+var oilprice = 500;
+var teaprice = 850;
+totalAmount = surfprice + oilprice + riceprice + teaprice;
+console.log(totalAmount);
+var urdubookprice = 1000;
+var computerbookprice = 900;
+var englishbookprice = 400;
+var islamiyatbookprice = 300;
+totalAmount = urdubookprice + computerbookprice + englishbookprice + islamiyatbookprice;
+console.log(totalAmount);
---
+++ add.ts
@@ -0,0 +1,17 @@
+// jab hum store ma enter howa hamara bill
+let totalAmount:number= 0;
+
+
+let urdubookprice:number = 1000
+let computerbookprice:number =900
+let englishbookprice : number = 400
+let islamiyatbookprice : number= 300
+
+totalAmount = urdubookprice + computerbookprice + englishbookprice + islamiyatbookprice;
+console.log (totalAmount);
+
+
+
+
+
+
